8682ea3fbec4a9e2e5da1942d6f63fb2530c674f
Derick Bitencourte da Silva <derickbit7@gmail.com>
2025-06-04 11:07:13 -0300

    ratelimit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,11 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +30,12 @@ class AppServiceProvider extends ServiceProvider
                if($this->app->environment('production')) {
             URL::forceScheme('https');
         }
+
+    RateLimiter::for('api', function (Request $request) {
+        return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+
+    });
+
         //        ResetPassword::createUrlUsing(function ($notifiable, $token) {
         //     return 'http://localhost:5173/redefinir-senha/' . $token . '?email=' . urlencode($notifiable->getEmailForPasswordReset());
         // });
